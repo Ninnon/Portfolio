f429882304f897da4d4f67b6aa61affd15add29e
Trenton Neilson <TNEILSON@1800contacts.com>
2018-09-02 18:51:16 -0600

    nav on bottom without error code in css file

--- Assets/site.js
+++ Assets/site.js
@@ -1,5 +1,5 @@
-$(document).ready(function () {
-    var canvas = document.getElementById("theCanvas");
-    canvas.width = $("#parent").width();
-    canvas.height = $("#parent").height();
-});
+// $(document).ready(function () {
+//     var canvas = document.getElementById("theCanvas");
+//     canvas.width = $("#parent").width();
+//     canvas.height = $("#parent").height();
+// });

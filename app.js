ae8f8262a23e82879026e321e5d0cb59a7f1a865
Trenton Neilson <trentonneilson14@hotmail.com>
2018-09-06 13:15:55 -0600

    test

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function(req,res) {
-res.render("index");
+res.render("index.ejs");
 });
 
 app.post("/contact", function(req, res) {

4cb83a0caec2a90e878d448caf283cdfbf54e40c
Trenton Neilson <trentonneilson14@hotmail.com>
2018-09-06 13:13:25 -0600

    change port

--- app.js
+++ app.js
@@ -41,6 +41,6 @@ app.post("/contact", function(req, res) {
 });
 
 
-app.listen(3000, function () {
+app.listen(process.env.PORT, process.env.IP, function () {
     console.log("Server started");
 });

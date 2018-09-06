19eb37d5549e72140fd2fea4efd674075f7ec56f
Trenton Neilson <trentonneilson14@hotmail.com>
2018-09-06 13:18:26 -0600

    testing

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var express = require("express");
 var app = express();
 var nodemailer = require("nodemailer");
 var bodyParser = require("body-parser");
-app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/public'));
+app.use(express.static("public"))
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));

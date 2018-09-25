652f2ab55d4f835442dead241f5a1aed15bd2dff
Ninnon <trentonneilson14@hotmail.com>
2018-09-24 22:10:57 -0600

    fix port

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.listen(process.env.PORT, process.env.IP, function () {
     console.log("Server started");
 });
 
-app.listen(3000, function () {
-    console.log("Server started");
-});
+// app.listen(3000, function () {
+//     console.log("Server started");
+// });
 
